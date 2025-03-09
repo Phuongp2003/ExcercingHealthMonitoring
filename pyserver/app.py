be28c6989b849f97842e59040190e0dfc76c327e
Phuongp2003 <phambuinamhphuong@gmail.com>
2025-03-09 13:25:55 +0700

    make change for download data

--- pyserver/app.py
+++ pyserver/app.py
@@ -1,5 +1,5 @@
 import socket
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from flask_socketio import SocketIO
 import logging
 import matplotlib.pyplot as plt
@@ -8,6 +8,7 @@ from server import Server
 from signal_processor import SignalProcessor
 from config import SERVER_PORT
 import csv
+import os
 
 # Initialize Flask app and SocketIO
 app = Flask(__name__)
@@ -19,9 +20,14 @@ logging.basicConfig(level=logging.DEBUG)
 # Global variable to store ESP connection info
 esp_info = {}
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
+@app.route('/')
+def index():
+    files = os.listdir('data')
+    return render_template('index.html', files=files)
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory('data', filename)
 
 # Global variable for CSV file write stream
 csv_file = None
@@ -30,7 +36,7 @@ csv_writer = None
 if __name__ == "__main__":
     # Initialize CSV file
     timestamp = time.strftime("%Y%m%d_%H%M%S")
-    csv_file = open(f"sensor_data_{timestamp}.csv", mode='w', newline='')
+    csv_file = open(f"data/sensor_data_{timestamp}.csv", mode='w', newline='')
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(["time", "irvalue", "redvalue"])
 
@@ -42,8 +48,7 @@ if __name__ == "__main__":
     plt.ion()
     plt.show()
 
-
-    socketio.run(app, port=5000)
+    socketio.run(app, port=5000, allow_unsafe_werkzeug=True)
     try:
         server.listen_for_data()
     except Exception as e:
